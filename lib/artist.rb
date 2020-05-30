eaedd4454182390531f7b456af14c73c1709bb29
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-30 21:40:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,39 @@
 class Artist
-  attr_accessor :name
+
+  attr_accessor :name, :songs
+  @@all =[]
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
- def songs
- Song.all.select do |song|
- song.artist == self
- end
- end
+  def add_song(song)
+    @songs << song
+  end
 
- def add_song(song)
- song.artist = self
- end
+  def save
+    @@all << self #saves the artist name to the all array
+  end
 
- def Artist.find_or_create_by_name(song_name)
- singer = Song.new(song_name)
- add_song(singer)
+  def self.all
+    @@all #displays the all array
+  end
 
- def print_songs
- Song.all.count
- end
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
 
- end
-
-
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
 end
