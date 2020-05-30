a1c683c7fb6c91b2295ccd422353f81d96179638
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-30 21:45:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
 
   attr_accessor :name, :artist
+@@all = [ ]
 
   def initialize(name)
     @name = name
@@ -8,7 +9,7 @@ class Song
 
 def self.all
   @@all
-end 
+end
 
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
