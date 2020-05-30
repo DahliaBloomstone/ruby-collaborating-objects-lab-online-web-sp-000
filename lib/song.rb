6372b2e68df63edcba19a2cb3e7918463669ec99
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-30 22:03:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
 @@all = [ ] #starts at an empty array for all song instances
 
   def initialize(name)
-    @name = name
+    @name = name #    sets the song name
     @@all << self #shovelling all new song instances into array @@all
   end
 
@@ -12,10 +12,10 @@ def self.all #returns all existing song instances
   @@all
 end
 
-  def self.new_by_filename(file_name)
+  def self.new_by_filename(file_name) #    creates a new instance of a song from the file that's passed
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
+    new_song = self.new(song) #    associates new song instance with the artist from the filename
     new_song.artist_name = artist
     new_song
   end
