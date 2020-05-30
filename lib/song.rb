56dd3452a53958d8b5f845bd78de9f8cbea62ef4
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-30 21:39:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :title, :artist 
+  attr_accessor :title, :artist
 
 
   def self.new_by_filename(filename)
@@ -13,5 +13,14 @@ end
 class MP3Importer
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  end
+
+  def artist_name=(name)
+      if (self.artist.nil?)
+        self.artist = Artist.new(name)
+      else
+        self.artist.name = name
+      end
+    end
   end
 end
