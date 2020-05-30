dcace7ae2e3a185587e87e9469ce8314035ad84c
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-30 21:55:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,17 +8,13 @@ class Artist
     @songs = []
   end
 
+  def save
+    @@all << self #saves the artist name to the all array
+  end
   def add_song(song)
     @songs << song
   end
 
-  def save
-    @@all << self #saves the artist name to the all array
-  end
-
-  def self.all
-    @@all #displays the all array
-  end
 
   def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
